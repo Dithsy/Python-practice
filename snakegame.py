b39b09a4f45cc02416eb79307f55c65ca8cc3e48
Eddith A <92625525+Dithsy@users.noreply.github.com>
2022-04-21 21:01:58 +0300

    Delete snakegame.py

--- snakegame.py
+++
@@ -1,8 +0,0 @@
-
-langauages = ['java','c','c++','python','javascript']
-
-for language in langauages [2:4]:
-    if language != 'python':
-        print(f'Are you sure you want to program in {language.title()}?')
-    else:
-        print(f'choose{language.upper()} instead')    
